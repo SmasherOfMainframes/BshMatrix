3d7484aebd7121e152374a7ea3fe2b51ad48d38c
GLaDOS <nem35b@protonmail.com>
2021-01-08 17:15:35 -0600

    asd

---
+++ cmake-build-debug/CMakeFiles/cRain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cRain.dir/cRain.c.o"
+  "cRain"
+  "cRain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cRain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
